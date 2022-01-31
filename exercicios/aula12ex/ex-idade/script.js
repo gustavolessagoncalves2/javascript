6b0f993ceebfefb1608c8770cd8c5a8e944aefa4
gustavolessagoncalves2 <gustavolessagoncalves2@gmail.com>
2022-01-31 13:35:54 -0300

    comecei o ex015 - idade

---
+++ exercicios/aula12ex/ex-idade/script.js
@@ -0,0 +1,30 @@
+//pegando os dados inseridos
+let campoVerificar = document.getElementById('inputVerificar')
+campoVerificar.addEventListener('click', verificar)
+
+let campoAnoDeNascimento = document.getElementById('inputAnoDeNascimento')
+
+let campoSexoMasculino = document.getElementById('checkboxMasculino').value
+
+let campoSexoFeminino = document.getElementById('checkboxFeminino').value
+
+
+let sexo = document.getElementById('formSexo').value
+
+
+//comparando os dados e mostrando resultados
+
+
+
+
+
+
+
+
+
+function verificar() {
+
+console.log(sexo)
+
+}
+
