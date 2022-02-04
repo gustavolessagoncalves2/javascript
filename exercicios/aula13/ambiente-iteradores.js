112cae9a79605cd83246630d34292d839f8c5ce8
Gustavo Lessa <86855871+gustavolessagoncalves2@users.noreply.github.com>
2022-02-04 13:42:30 -0300

    Create ambiente-iteradores.js

---
+++ exercicios/aula13/ambiente-iteradores.js
@@ -0,0 +1,15 @@
+let contador = 1 //variável que vai realizar a contagem das aparições
+
+//Estrutura de repetição com teste lógico no início
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+//Estrutura de repetição com teste lógico no final
+do {
+    console.log(`Contagem ${contador}`)
+    contador++
+} while (contador <= 12)
+
+
