b77028fe8d597fd37912b342a31b48dbbdb8b93a
Gustavo Lessa <86855871+gustavolessagoncalves2@users.noreply.github.com>
2022-02-06 02:18:04 -0300

    versão 01 ex017

---
+++ exercicios/aula14ex/ex017-tabuada/script.js
@@ -0,0 +1,25 @@
+buttonGerarTabuada = document.querySelector("input#gerarTabuada")
+buttonGerarTabuada.addEventListener('click', gerarTabuada)
+
+function gerarTabuada() {
+    var txtNum = document.querySelector("input#num")
+    var txtRes = document.querySelector("div#res")
+    var txtTab = document.querySelector("select#tabelaTabuada")
+    
+    var num = Number(txtNum.value)
+    
+    if (txtNum.value.length == 0) {
+        alert("insira um número")
+    } else {
+    
+        for (multiplicando = 0; multiplicando <= 10; multiplicando++) {
+            
+            var produto = num * multiplicando
+
+            txtTab.innerHTML += `<option value="${produto}">${num} x ${multiplicando} = ${produto} </option>`
+            
+            txtTab.setAttribute("size", `${multiplicando + 1}`)
+        }
+    
+    }
+}
