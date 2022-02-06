de956a266447bb61c5d37d1a429e913436775566
Gustavo Lessa <86855871+gustavolessagoncalves2@users.noreply.github.com>
2022-02-06 01:11:52 -0300

    finalizado ex016
    finalizei ex016 sobre o Super Contador

--- exercicios/aula14ex/ex016-vamos-contar/script.js
+++ exercicios/aula14ex/ex016-vamos-contar/script.js
@@ -3,26 +3,38 @@ buttonContar.addEventListener('click', contar)
 
 function contar() {
 
-    var inicio = parseInt(document.querySelector("input#inicio").value)
-    
-    var fim = parseInt(document.querySelector("input#fim").value)
-    
-    var passo = parseInt(document.querySelector("input#passo").value)
-    
+    var txtInicio = document.querySelector("input#inicio")
+    var txtFim = document.querySelector("input#fim")
+    var txtPasso = document.querySelector("input#passo")
     var txtRes = document.querySelector("div#res")
 
-    if (inicio == "") {
-        alert("Preencha todos os campos")
-    } else if (passo == 0) {
-        alert("Passo = 0 é invalido. Considerando Passo = 1")
-        passo = 1
+    var inicio = Number(txtInicio.value)
+    var fim = Number(txtFim.value)
+    var passo = Number(txtPasso.value)
+
+    if (txtInicio.value.length == 0 || txtFim.value.length == 0 || txtPasso.value.length == 0) {
+        txtRes.innerHTML = `Impossível contar. Preencha todos os campos.`
     } else {
-        txtRes.innerHTML = `Contagem: <br>`
-        for (num = inicio; num <= fim ; num = num + passo) {
-            txtRes.innerHTML += `${num} &#x1F449;`
+        txtRes.innerHTML = `Contagem: <br>` 
+        if (passo <= 0) {
+            alert("Passo = 0 é invalido. Considerando Passo = 1")
+            passo = 1
         }
+        if (inicio < fim) { 
+            //contagem crescente
+            for (num = inicio; num <= fim ; num += passo) {
+            txtRes.innerHTML += `${num} &#x1F449;`
+            }
+        } else {
+            //contagem decrescente
+            for (num = inicio; num <= fim ; num += passo) {
+                txtRes.innerHTML += `${num} &#x1F449;`
+            }
+        } 
         txtRes.innerHTML += `&#x1F3C1;`
     }
+    
+    
     
 }
 
