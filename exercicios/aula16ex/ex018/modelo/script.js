3d033a3647fce73802778408790ce6fd33387921
Gustavo Lessa <86855871+gustavolessagoncalves2@users.noreply.github.com>
2022-02-07 18:23:37 -0300

    Update script.js

--- exercicios/aula16ex/ex018/modelo/script.js
+++ exercicios/aula16ex/ex018/modelo/script.js
@@ -1,11 +1,19 @@
-inputAdicionar = document.querySelector('input#btAdicionar')
+//Adicionando os Ouvidores
+let inputAdicionar = document.querySelector('input#btAdicionar')
 inputAdicionar.addEventListener('click', adicionar)
-
-inputFinalizar = document.querySelector('input#btFinalizar')
+let inputFinalizar = document.querySelector('input#btFinalizar')
 inputFinalizar.addEventListener('click', finalizar)
 
+//Pegando os valores dos Inputs e tratando-os
+var txtNumber = document.querySelector('input#txtNumber')
+var number = parseInt(txtNumber.value)
+
+
+
+
 function adicionar() {
-    alert('adicionou')
+    //alert('adicionou')
+    alert(number)
 }
 
 function finalizar() {
