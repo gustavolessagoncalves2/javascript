2a262c79e21a82757018b1f1125b92662e9e88fb
Gustavo Lessa <86855871+gustavolessagoncalves2@users.noreply.github.com>
2022-02-07 17:10:51 -0300

    iniciado ex018

---
+++ exercicios/aula16ex/ex018/modelo/script.js
@@ -0,0 +1,13 @@
+inputAdicionar = document.querySelector('input#btAdicionar')
+inputAdicionar.addEventListener('click', adicionar)
+
+inputFinalizar = document.querySelector('input#btFinalizar')
+inputFinalizar.addEventListener('click', finalizar)
+
+function adicionar() {
+    alert('adicionou')
+}
+
+function finalizar() {
+    alert('finalizou')
+}
